7e44fe1f353528f962202172d4b5928c9340f7ce
Sophie Bosio <65249932+SophieBosio@users.noreply.github.com>
2026-05-05 17:52:12 +0200

    Import fonts as top-level stylesheet instead of with @import

--- src/frontpage.clj
+++ src/frontpage.clj
@@ -28,7 +28,10 @@
 (defn headers []
   (list
    [:meta {:charset "utf-8"}]
-   [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]))
+   [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+   [:link {:rel "preconnect" :href "https://fonts.googleapis.com"}]
+   [:link {:rel "preconnect" :href "https://fonts.gstatic.com" :crossorigin ""}]
+   [:link {:rel "stylesheet" :href "https://fonts.googleapis.com/css2?family=Roboto&family=Roboto+Mono&display=swap"}]))
 
 (defn asset-headers []
   (assets/load-all :assets.style/layout
