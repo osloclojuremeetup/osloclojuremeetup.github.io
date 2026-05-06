b914d1797d5d01699939993932c2d2ce0e9591b7
Teodor Heggelund <git@teod.eu>
2026-05-06 22:46:23 +0200

    Prefer links to personal websites for speakers

--- src/components.clj
+++ src/components.clj
@@ -1,10 +1,22 @@
 (ns components)
 
-(defn speaker [speaker]
-  (cond (and (:person/name speaker)
-             (:github/id speaker))
-        [:a {:href (str "https://github.com/" (:github/id speaker))}
-         (:person/name speaker)]
+(defn find-speaker-website [speaker]
+  (or (-> speaker :person/website :site/url)
+      (when (:github/id speaker)
+        (str "https://github.com/" (:github/id speaker)))))
 
-        (:person/name speaker)
-        (:person/name speaker)))
+(defn speaker [speaker]
+  (when (:person/name speaker)
+    (or (when-let [website (find-speaker-website speaker)]
+          [:a {:href website} (:person/name speaker)])
+
+        (:person/name speaker))))
+
+(comment
+  (set! *print-namespace-maps* false)
+  (require '[datomic.api :as d])
+  (def magnar (d/entity @state/!db [:github/id "magnars"]))
+  (speaker magnar)
+
+  (-> magnar :person/website :site/url)
+  )
